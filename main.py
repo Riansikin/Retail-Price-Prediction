61a86913ce31d608d73a0f315a9ef57b46c1ba03
Muhammad Wahyu Sikin Febriansyah <riansikin7@gmail.com>
2025-05-23 22:51:33 +0700

    Update Page

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="ASG", layout="wide", page_icon=":star:")
+st.set_page_config(page_title="Retail Price Prediction", layout="wide", page_icon=":star:")
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Info", ["About Me", "Prediction"])
